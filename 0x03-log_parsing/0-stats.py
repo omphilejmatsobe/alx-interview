df69053003a85d493c72dccb609522aaa6143eb7
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-03-29 00:52:05 +0200

    File test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,48 +2,39 @@
 """
 reads stdin line by line and computes metrics
 """
+
 import sys
-import re
 
 
-def parse(log: dict) -> None:
-    """
-    helper function to display stats
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+if __name__ == '__main__':
 
+    size, counter = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-if __name__ == "__main__":
-    regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
-
-    counter = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
+    def print_stats(stats: dict, size: int) -> None:
+        print("File size: {:d}".format(size))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
         for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
-                counter += 1
-                code = match.group(1)
-                size = int(match.group(2))
-
-                # File size
-                log["file_size"] += size
-
-                # status code
-                if (code.isdecimal()):
-                    log["code_frequency"][code] += 1
-
-                if (counter % 10 == 0):
-                    output(log)
-    finally:
-        pars(log)
+            counter += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                size += int(data[-1])
+            except BaseException:
+                pass
+            if counter % 10 == 0:
+                print_stats(stats, size)
+        print_stats(stats, size)
+    except KeyboardInterrupt:
+        print_stats(stats, size)
+        raise
