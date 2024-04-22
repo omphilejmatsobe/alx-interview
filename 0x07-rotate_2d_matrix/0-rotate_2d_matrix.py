8ea76238a6e307e601aaf3e907c5daad4a7ff2d1
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-04-22 15:15:51 +0200

    Matrix test

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Rotates 2-D matrix"""
+
+
+def rotate_2d_matrix(m):
+    """Arranges and Rotates a matrix in-place"""
+    
+    x = len(m)
+    temp1, temp2 = 0, 0
+
+    for j in range(0, len(m) // 2 + 1):
+        for i in range(j, (x - 1)):
+            temp1 = m[i][(x - 1)]
+            m[i][x - 1] = m[j][i]
+
+            temp2 = m[x - 1][x - 1 - i + j]
+            m[x - 1][x - 1 - i + j] = temp1
+
+            temp1 = m[x - 1 - i + j][j]
+            m[x - 1 - i + j][j] = temp2
+
+            m[j][i] = temp1
+
+        n -= 1
